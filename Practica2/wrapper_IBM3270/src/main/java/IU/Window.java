4be6e61d320373af643afcfb307a8565f352f56f
jtambo99 <jtambo@live.com>
2020-10-18 10:21:13 +0200

    Base preparada para conexion con 3270

--- Practica2/wrapper_IBM3270/src/main/java/IU/Window.java
+++ Practica2/wrapper_IBM3270/src/main/java/IU/Window.java
@@ -1,9 +1,18 @@
 package IU;
 
+import Render.TextRenderer;
+import Scrapper.Field;
+import Scrapper.S3270;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import static Scrapper.S3270.TerminalMode.MODE_80_24;
+import static Scrapper.S3270.TerminalType.TYPE_3278;
 
 public class Window extends JFrame {
 
@@ -15,6 +24,7 @@ public class Window extends JFrame {
         // Main Container (top-level)
         Container cp = getContentPane();
         cp.setLayout(new GridLayout(2, 1, 0, 5));
+
 
         // General Task button
         generalTask = new JButton();
@@ -50,7 +60,39 @@ public class Window extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    public static String getScreenText(S3270 s3270) {
+        return new TextRenderer().render(s3270.getScreen());
+    }
+
     public static void main(String[] args) {
+
         new Window();
+        //Connection with S3270
+        System.out.println("->Conexion con mainframe.");
+
+        S3270 prueba = new S3270("s3270", "155.210.152.51", 101, TYPE_3278, MODE_80_24);
+        System.out.println("->Conexion establecida.");
+
+        prueba.enter();
+        prueba.enter();
+
+
+        System.out.println("*---------------*");
+        prueba.updateScreen();
+
+
+//        System.out.println("->HOLA");
+
+        prueba.tab();
+        prueba.type("Grupo_02");
+        prueba.tab();
+        prueba.type("secreto6");
+//        prueba.enter();
+        prueba.submitScreen();
+        prueba.updateScreen();
+
+        System.out.println("*---------------*");
+        String out = getScreenText(prueba);
+        System.out.println(out);
     }
 }
