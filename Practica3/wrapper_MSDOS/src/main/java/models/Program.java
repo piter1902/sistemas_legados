7cbb7a2564fe1e2533ef5861f185ef6a9c4b1f17
jtambo99 <jtambo@live.com>
2020-10-21 18:31:27 +0200

    Añado modelsssssss

---
+++ Practica3/wrapper_MSDOS/src/main/java/models/Program.java
@@ -0,0 +1,57 @@
+package models;
+
+public class Program {
+    private String name;
+    private String type;
+    private String tape;
+    private String registry;
+
+    public Program(String name, String type, String tape, String registry) {
+        this.name = name;
+        this.type = type;
+        this.tape = tape;
+        this.registry = registry;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTape() {
+        return tape;
+    }
+
+    public void setTape(String tape) {
+        this.tape = tape;
+    }
+
+    public String getRegistry() {
+        return registry;
+    }
+
+    public void setRegistry(String registry) {
+        this.registry = registry;
+    }
+
+    @Override
+    public String toString() {
+        return "Program{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", tape='" + tape + '\'' +
+                ", registry='" + registry + '\'' +
+                '}';
+    }
+}
